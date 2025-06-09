4a5efb12fb603bdb997136ef185ed17291a6c5be
WEBZaytsev <nikita@zaitsv.dev>
2025-06-10 02:31:59 +0300

    Add translations.js script and remove hardcoded translation data from index.html to streamline localization management and improve code maintainability.

---
+++ translations.js
@@ -0,0 +1,494 @@
+const MESSAGES = {
+  en: {
+    support: "Support",
+    system: "System",
+    light: "Light",
+    dark: "Dark",
+    name: "Name",
+    status: "Status",
+    active: "Active",
+    limited: "Limited",
+    expired: "Expired",
+    disabled: "Disabled",
+    duration: "Duration",
+    until: "Until",
+    forever: "Forever",
+    traffic: "Traffic",
+    subscription: "Subscription",
+    getLink: "Get link",
+    getLinkModalTitle: "Scan the QR code above in the client",
+    getLinkModalSubtitle:
+      "Simple subscription addition in any client. Another option: copy the link below and paste it into the client",
+    copyLink: "Copy link",
+    copied: "Copied",
+    guide: "Guide",
+    pc: "PC",
+    openInApp: "Open in {app}",
+    addSubscription: "Add subscription",
+    clashVerge: "⭐ Hiddify",
+    happAlpha: "⭐ Happ",
+    comingSoon: "Coming soon…",
+    separateLinksTitle: "Available Servers List",
+    separateLinks1:
+      "In this section, you can access your configurations individually, which allows you to share each of them using a QR code or simply copy them",
+    separateLinks2:
+      "May be useful if, for example, you need to set up a connection only to a specific server on a particular device",
+    guideContent: {
+      android: {
+        first: {
+          title: "Install and open Hiddify",
+          text: "Open the page in Google Play and install the app. Launch it, select the <b>Russia</b> region, and then press <b>Start</b>.",
+        },
+        second: {
+          title: "Add the subscription",
+          text: "Press the button below — the app will open, and the subscription will be added automatically.",
+        },
+        third: {
+          title: "Connect and enjoy",
+          text: "In the main section, press the large power button in the center to connect to the VPN. If needed, select another server by tapping on the «>» icon at the bottom. Enjoy :)",
+        },
+      },
+      ios: {
+        first: {
+          title: "Install and open Hiddify",
+          text: "Open the page in App Store and install the app. Launch it, tap <b>Allow</b> in the VPN configuration permission window, and enter your passcode. Select the <b>Russia</b> region, and then press <b>Start</b>.",
+        },
+        second: {
+          title: "Add the subscription",
+          text: "Press the button below and in the pop-up window press <b>Open</b> — the app will open, and the subscription will be added automatically.",
+        },
+        third: {
+          title: "Connect and enjoy",
+          text: "In the main section, press the large power button in the center to connect to the VPN. If needed, select another server by tapping on the «>» icon at the bottom. Enjoy :)",
+        },
+      },
+      pc: {
+        first: {
+          title: "Install and open Hiddify",
+          text: "Install the app. Launch it, select the <b>Russia</b> region, and then press <b>Start</b>.",
+        },
+        second: {
+          title: "Add the subscription",
+          text: "Press the button below — the app will open, and the subscription will be added automatically. If this doesn't happen, close the app and try again.",
+        },
+        third: {
+          title: "Connect and enjoy",
+          text: "In the main section, press the large power button in the center to connect to the VPN. If needed, select another server in the <b>Proxy</b> section. Enjoy :)",
+        },
+      },
+      happ: {
+        android: {
+          first: {
+            title: "Install and open Happ",
+            text: "Open the page in Google Play and install the app. Or install the app directly from the APK file if Google Play is not working.",
+          },
+          second: {
+            title: "Add the subscription",
+            text: "Click the button below to add subscription",
+          },
+          third: {
+            title: "Connect and use",
+            text: "Open the app and connect to the server",
+          },
+        },
+        ios: {
+          first: {
+            title: 'Install and open Happ',
+            text: 'Open the page in App Store and install the app. Launch it, in the VPN configuration permission window click Allow and enter your passcode.',
+          },
+          second: {
+            title: 'Add the subscription',
+            text: 'Click the button below — the app will open and the subscription will be added automatically',
+          },
+          third: {
+            title: 'Connect and use',
+            text: "In the main section, click the large power button in the center to connect to VPN. Don't forget to select a server from the server list. If needed, choose another server from the server list.",
+          },
+        },
+        pc: {
+          first: {
+            title: 'Install and open Happ',
+            text: 'Choose your operating system, download and install the application.',
+          },
+          second: {
+            title: 'Add the subscription',
+            text: 'Click the button below — the app will open and the subscription will be added automatically',
+          },
+          third: {
+            title: 'Connect and use',
+            text: 'In the main section, click the large power button in the center to connect to VPN.',
+          },
+        },
+      },
+    },
+  },
+  ru: {
+    support: "Поддержка",
+    system: "Системная",
+    light: "Светлая",
+    dark: "Темная",
+    name: "Имя",
+    status: "Статус",
+    active: "Активна",
+    limited: "Ограничена",
+    expired: "Истекла",
+    disabled: "Отключена",
+    duration: "Срок",
+    until: "До",
+    forever: "Навсегда",
+    traffic: "Трафик",
+    subscription: "Подписка",
+    getLink: "Получить ссылку",
+    getLinkModalTitle: "Отсканируй QR-код выше в клиенте",
+    getLinkModalSubtitle:
+      "Простое добавление подписки в любой клиент. Есть и другой вариант: скопируй ссылку ниже и вставь в клиент",
+    copyLink: "Скопировать ссылку",
+    copied: "Скопировано",
+    guide: "Инструкция",
+    pc: "ПК",
+    openInApp: "Открыть в {app}",
+    addSubscription: "Добавить подписку",
+    clashVerge: "⭐ Hiddify",
+    happAlpha: "⭐ Happ",
+    comingSoon: "Coming soon…",
+    separateLinksTitle: "Список доступных серверов",
+    separateLinks1:
+      "В этом разделе вы получаете доступ к своим конфигурациям по отдельности, что позволяет делиться каждой из них с помощью QR-кода или просто копировать их",
+    separateLinks2:
+      "Может пригодиться, если, например, нужно настроить подключение только к конкретному серверу на определенном устройстве",
+    guideContent: {
+      android: {
+        first: {
+          title: "Установи и открой Hiddify",
+          text: "Открой страницу в Google Play и установи приложение. Запусти его, выбери регион <b>Россия</b>, а затем нажми <b>Старт</b>.",
+        },
+        second: {
+          title: "Добавь подписку",
+          text: "Нажми кнопку ниже — приложение откроется, и подписка добавится автоматически.",
+        },
+        third: {
+          title: "Подключись и пользуйся",
+          text: "В главном разделе нажми большую кнопку включения в центре для подключения к VPN. При необходимости выбери другой сервер, нажав на «>» внизу. Пользуйся :)",
+        },
+      },
+      ios: {
+        first: {
+          title: "Установи и открой Hiddify",
+          text: "Открой страницу в App Store и установи приложение. Запусти его, в окне разрешения на добавление конфигураций VPN нажми <b>Разрешить</b> и введи свой код-пароль. Выбери регион <b>Россия</b>, а затем нажми <b>Старт</b>.",
+        },
+        second: {
+          title: "Добавь подписку",
+          text: "Нажми на кнопку ниже и в появившемся окне нажми <b>Открыть</b> — откроется приложение, и подписка добавится автоматически.",
+        },
+        third: {
+          title: "Подключись и пользуйся",
+          text: "В главном разделе нажми большую кнопку включения в центре для подключения к VPN. При необходимости выбери другой сервер, нажав на «>» внизу. Пользуйся :)",
+        },
+      },
+      pc: {
+        first: {
+          title: "Установи и открой Hiddify",
+          text: "Установи приложение. Запусти его, выбери регион <b>Россия</b>, а затем нажми <b>Старт</b>.",
+        },
+        second: {
+          title: "Добавь подписку",
+          text: "Нажми кнопку ниже — приложение откроется, и подписка добавится автоматически. Если это не произошло, закрой приложение и попробуй снова.",
+        },
+        third: {
+          title: "Подключись и пользуйся",
+          text: "В главном разделе нажми большую кнопку включения в центре для подключения к VPN. При необходимости выбери другой сервер в разделе <b>Прокси</b>. Пользуйся :)",
+        },
+      },
+      happ: {
+        android: {
+          first: {
+            title: "Установи и открой Happ",
+            text: "Открой страницу в Google Play и установи приложение. Или установи приложение из APK файла напрямую, если Google Play не работает.",
+          },
+          second: {
+            title: "Добавь подписку",
+            text: "Нажми кнопку ниже, чтобы добавить подписку",
+          },
+          third: {
+            title: "Подключись и используй",
+            text: "Открой приложение и подключись к серверу",
+          },
+        },
+        ios: {
+          first: {
+            title: 'Установи и открой Happ',
+            text: 'Откройте страницу в App Store и установите приложение. Запустите его, в окне разрешения VPN-конфигурации нажмите Allow и введите свой пароль.',
+          },
+          second: {
+            title: 'Добавь подписку',
+            text: 'Нажмите кнопку ниже — приложение откроется, и подписка добавится автоматически.',
+          },
+          third: {
+            title: 'Подключись и используй',
+            text: 'В главном разделе нажмите большую кнопку включения в центре для подключения к VPN. Не забудьте выбрать сервер в списке серверов. При необходимости выберите другой сервер из списка серверов.',
+          },
+        },
+        pc: {
+          first: {
+            title: 'Установи и открой Happ',
+            text: 'Выберите вашу операционную систему, скачайте и установите приложение.',
+          },
+          second: {
+            title: 'Добавь подписку',
+            text: 'Нажмите кнопку ниже — приложение откроется, и подписка добавится автоматически.',
+          },
+          third: {
+            title: 'Подключись и используй',
+            text: 'В главном разделе нажмите большую кнопку включения в центре для подключения к VPN.',
+          },
+        },
+      },
+    },
+  },
+  zh: {
+    support: "支持",
+    system: "系统",
+    light: "浅色",
+    dark: "深色",
+    name: "名字",
+    status: "状态",
+    active: "激活",
+    limited: "受限",
+    expired: "已过期",
+    disabled: "已禁用",
+    duration: "持续时间",
+    until: "直到",
+    forever: "永久",
+    traffic: "流量",
+    subscription: "订阅",
+    getLink: "获取链接",
+    getLinkModalTitle: "在客户端扫描上方二维码",
+    getLinkModalSubtitle:
+      "简单地在任何客户端添加订阅。还有另一种方法：复制下面的链接并粘贴到客户端中。",
+    copyLink: "复制链接",
+    copied: "已复制",
+    guide: "指南",
+    pc: "电脑",
+    openInApp: "在 {app} 中打开",
+    addSubscription: "添加订阅",
+    clashVerge: "⭐ Hiddify",
+    happAlpha: "⭐ Happ",
+    comingSoon: "Coming soon…",
+    separateLinksTitle: "可用服务器列表",
+    separateLinks1:
+      "在此部分中，您可以单独访问您的配置，这允许您使用QR码共享它们或简单地复制它们",
+    separateLinks2:
+      "如果您需要在特定设备上仅连接到特定服务器，这可能会很有用",
+    guideContent: {
+      android: {
+        first: {
+          title: "安装并打开 Hiddify",
+          text: `在 Google Play 页面打开并安装应用程序。启动应用，选择区域 <b>俄罗斯</b>，然后点击 <b>开始</b>。`,
+        },
+        second: {
+          title: "添加订阅",
+          text: `点击下方按钮 — 应用程序将打开并自动添加订阅。如果未成功，请关闭应用程序并重试。`,
+        },
+        third: {
+          title: "连接并享受",
+          text: `在主界面点击中心的大开关按钮以连接到 VPN。如有需要，可以通过点击底部的「>」图标选择其他服务器。尽情享受 :)`,
+        },
+      },
+      ios: {
+        first: {
+          title: "安装并打开 Hiddify",
+          text: `在 App Store 页面打开并安装应用程序。启动后，在 VPN 配置权限窗口点击 <b>允许</b>，然后输入您的密码。选择区域 <b>俄罗斯</b>，然后点击 <b>开始</b>。`,
+        },
+        second: {
+          title: "添加订阅",
+          text: `点击下方按钮，在弹出窗口中点击 <b>打开</b> — 应用程序将打开并自动添加订阅。如果未成功，请关闭应用程序并重试。`,
+        },
+        third: {
+          title: "连接并享受",
+          text: `在主界面点击中心的大开关按钮以连接到 VPN。如有需要，可以通过点击底部的「>」图标选择其他服务器。尽情享受 :)`,
+        },
+      },
+      pc: {
+        first: {
+          title: "安装并打开 Hiddify",
+          text: `安装应用程序。启动应用，选择区域 <b>俄罗斯</b>，然后点击 <b>开始</b>。`,
+        },
+        second: {
+          title: "添加订阅",
+          text: `点击下方按钮 — 应用程序将打开并自动添加订阅。如果未成功，请关闭应用程序并重试。`,
+        },
+        third: {
+          title: "连接并享受",
+          text: `在主界面点击中心的大开关按钮以连接到 VPN。如有需要，可以在<b>代理</b>部分选择其他服务器。尽情享受 :)`,
+        },
+      },
+      happ: {
+        android: {
+          first: {
+            title: "安装并打开 Happ",
+            text: "在 Google Play 页面打开并安装应用程序。启动应用，选择区域 <b>俄罗斯</b>，然后点击 <b>开始</b>。",
+          },
+          second: {
+            title: "添加订阅",
+            text: "点击下方按钮 — 应用程序将打开并自动添加订阅。如果未成功，请关闭应用程序并重试。",
+          },
+          third: {
+            title: "连接并使用",
+            text: "打开应用程序并连接到服务器",
+          },
+        },
+        ios: {
+          first: {
+            title: '安装并打开 Happ',
+            text: '打开 App Store 页面并安装应用。启动它，在 VPN 配置权限窗口中点击允许并输入您的密码。',
+          },
+          second: {
+            title: '添加订阅',
+            text: '点击下面的按钮 — 应用程序将打开，订阅将自动添加。',
+          },
+          third: {
+            title: '连接和使用',
+            text: '在主区域，点击中央的大电源按钮连接到 VPN。不要忘记从服务器列表中选择一个服务器。如果需要，从服务器列表中选择另一个服务器。',
+          },
+        },
+        pc: {
+          first: {
+            title: '安装并打开 Happ',
+            text: '选择您的操作系统，下载并安装应用程序。',
+          },
+          second: {
+            title: '添加订阅',
+            text: '点击下面的按钮 — 应用程序将打开，订阅将自动添加。',
+          },
+          third: {
+            title: '连接和使用',
+            text: '在主区域，点击中央的大电源按钮连接到 VPN。',
+          },
+        },
+      },
+    },
+  },
+  fa: {
+    support: "پشتیبانی",
+    system: "سیستم",
+    light: "روشن",
+    dark: "تاریک",
+    name: "نام",
+    status: "وضعیت",
+    active: "فعال",
+    limited: "محدود",
+    expired: "منقضی‌شده",
+    disabled: "غیرفعال",
+    duration: "مدت زمان",
+    until: "تا",
+    forever: "برای همیشه",
+    traffic: "ترافیک",
+    subscription: "اشتراک",
+    getLink: "دریافت لینک",
+    getLinkModalTitle: "کد QR بالا را در برنامه اسکن کنید",
+    getLinkModalSubtitle:
+      "افزودن اشتراک به سادگی در هر برنامه‌ای. گزینه دیگر: لینک زیر را کپی کرده و در برنامه جای‌گذاری کنید",
+    copyLink: "کپی لینک",
+    copied: "کپی شد",
+    guide: "راهنما",
+    pc: "کامپیوتر",
+    openInApp: "باز کردن در {app}",
+    addSubscription: "افزودن اشتراک",
+    clashVerge: "⭐ Hiddify",
+    happAlpha: "⭐ Happ",
+    comingSoon: "Coming soon…",
+    separateLinksTitle: "لیست سرورهای موجود",
+    separateLinks1:
+      "در این بخش، شما می‌توانید به پیکربندی‌های خود به صورت جداگانه دسترسی داشته باشید که به شما امکان می‌دهد هر یک از آنها را با استفاده از کد QR به اشتراک بگذارید یا به سادگی آنها را کپی کنید",
+    separateLinks2:
+      "ممکن است مفید باشد اگر، به عنوان مثال، نیاز به راه‌اندازی اتصال تنها به یک سرور خاص در یک دستگاه خاص داشته باشید",
+    guideContent: {
+      android: {
+        first: {
+          title: "نصب و باز کردن Hiddify",
+          text: "صفحه را در گوگل پلی باز کرده و برنامه را نصب کنید. آن را اجرا کنید، منطقه <b>روسیه</b> را انتخاب کرده و سپس روی <b>شروع</b> کلیک کنید.",
+        },
+        second: {
+          title: "افزودن اشتراک",
+          text: "روی دکمه زیر کلیک کنید — برنامه باز می‌شود و اشتراک به‌طور خودکار اضافه می‌شود.",
+        },
+        third: {
+          title: "اتصال و لذت بردن",
+          text: "در بخش اصلی، روی دکمه بزرگ روشن/خاموش در مرکز کلیک کنید تا به VPN متصل شوید. در صورت نیاز، با ضربه زدن روی نماد «>» در پایین، سرور دیگری را انتخاب کنید. لذت ببرید :)",
+        },
+      },
+      ios: {
+        first: {
+          title: "نصب و باز کردن Hiddify",
+          text: "صفحه را در اپ استور باز کرده و برنامه را نصب کنید. آن را اجرا کنید، در پنجره مجوز پیکربندی VPN روی <b>اجازه دادن</b> کلیک کنید و گذرواژه خود را وارد کنید. منطقه <b>روسیه</b> را انتخاب کرده و سپس روی <b>شروع</b> کلیک کنید.",
+        },
+        second: {
+          title: "افزودن اشتراک",
+          text: "روی دکمه زیر کلیک کنید و در پنجره پاپ‌آپ روی <b>باز کردن</b> کلیک کنید — برنامه باز می‌شود و اشتراک به‌طور خودکار اضافه می‌شود.",
+        },
+        third: {
+          title: "اتصال و لذت بردن",
+          text: "در بخش اصلی، روی دکمه بزرگ روشن/خاموش در مرکز کلیک کنید تا به VPN متصل شوید. در صورت نیاز، با ضربه زدن روی نماد «>» در پایین، سرور دیگری را انتخاب کنید. لذت ببرید :)",
+        },
+      },
+      pc: {
+        first: {
+          title: "نصب و باز کردن Hiddify",
+          text: "برنامه را نصب کنید. آن را اجرا کنید، منطقه <b>روسیه</b> را انتخاب کرده و سپس روی <b>شروع</b> کلیک کنید.",
+        },
+        second: {
+          title: "افزودن اشتراک",
+          text: "روی دکمه زیر کلیک کنید — برنامه باز می‌شود و اشتراک به‌طور خودکار اضافه می‌شود. اگر این اتفاق نیفتاد، برنامه را ببندید و دوباره امتحان کنید.",
+        },
+        third: {
+          title: "اتصال و لذت بردن",
+          text: "در بخش اصلی، روی دکمه بزرگ روشن/خاموش در مرکز کلیک کنید تا به VPN متصل شوید. در صورت نیاز، در بخش <b>پروکسی</b> سرور دیگری را انتخاب کنید. لذت ببرید :)",
+        },
+      },
+      happ: {
+        android: {
+          first: {
+            title: "نصب و باز کردن Happ",
+            text: "صفحه را در گوگل پلی باز کرده و برنامه را نصب کنید. آن را اجرا کنید، منطقه <b>روسیه</b> را انتخاب کرده و سپس روی <b>شروع</b> کلیک کنید.",
+          },
+          second: {
+            title: "افزودن اشتراک",
+            text: "روی دکمه زیر کلیک کنید — برنامه باز می‌شود و اشتراک به‌طور خودکار اضافه می‌شود.",
+          },
+          third: {
+            title: "اتصال و لذت بردن",
+            text: "در بخش اصلی، روی دکمه بزرگ روشن/خاموش در مرکز کلیک کنید تا به VPN متصل شوید. در صورت نیاز، با ضربه زدن روی نماد «>» در پایین، سرور دیگری را انتخاب کنید. لذت ببرید :)",
+          },
+        },
+        ios: {
+          first: {
+            title: 'نصب و باز کردن Happ',
+            text: 'صفحه را در اپ استور باز کرده و برنامه را نصب کنید. آن را اجرا کنید، در پنجره مجوز پیکربندی VPN روی Allow کلیک کرده و رمز عبور خود را وارد کنید.',
+          },
+          second: {
+            title: 'افزودن اشتراک',
+            text: 'روی دکمه زیر کلیک کنید — برنامه باز می‌شود و اشتراک به‌طور خودکار اضافه می‌شود.',
+          },
+          third: {
+            title: 'اتصال و استفاده',
+            text: 'در بخش اصلی، دکمه بزرگ پاور در مرکز را برای اتصال به VPN فشار دهید. فراموش نکنید که یک سرور از لیست سرورها انتخاب کنید. در صورت لزوم، سرور دیگری را از لیست سرورها انتخاب کنید.',
+          },
+        },
+        pc: {
+          first: {
+            title: 'نصب و باز کردن Happ',
+            text: 'سیستم عامل خود را انتخاب کنید، برنامه را دانلود و نصب کنید.',
+          },
+          second: {
+            title: 'افزودن اشتراک',
+            text: 'روی دکمه زیر کلیک کنید — برنامه باز می‌شود و اشتراک به‌طور خودکار اضافه می‌شود.',
+          },
+          third: {
+            title: 'اتصال و استفاده',
+            text: 'در بخش اصلی، دکمه بزرگ پاور در مرکز را برای اتصال به VPN فشار دهید.',
+          },
+        },
+      },
+    },
+  },
+}; 
